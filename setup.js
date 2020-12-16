3c543654171c662e6c9c694086ef2da493147b93
Sam Green <ssfg@MacBook-Pro.local>
2020-12-16 12:24:21 +0000

    updating main to have countTypeOnPlane, and random error messages for failed tests

--- setup.js
+++ setup.js
@@ -30,7 +30,16 @@ function onComplete(result, level) {
   let testResults = {}
   if(result instanceof Array) {
    for(let i=0; i < result.length; i++) {
-     testResults[i+1] = {pass:result[i]}
+     if(result[i]) {
+      testResults[i+1] = {pass:result[i]}
+     } else {
+      const errorMessages = ["Not quite", "Try again", "This one needs a bit more work", "Have another go"];
+      const random = Math.floor(Math.random() * errorMessages.length);
+      testResults[i+1] = {
+        pass: result[i],
+        errorMessage: errorMessages[random]
+      }
+     }     
     } 
   } else {
 		testResults = {
