f0ee91ffe0b26a5af8275af599710fb9931905d1
henrylake <hlake@turinglab.co.uk>
2021-01-05 13:01:05 +0000

    update setup file

--- setup.js
+++ setup.js
@@ -21,11 +21,11 @@ function onComplete(result, level) {
         if(result[i] == false) {
           completed = false
         }
-      } 
+      }
   } else {
 		completed = result
   }
-  
+
   // send corresponding test results
   let testResults = {}
   if(result instanceof Array) {
@@ -39,14 +39,14 @@ function onComplete(result, level) {
         pass: result[i],
         errorMessage: errorMessages[random]
       }
-     }     
-    } 
+     }
+    }
   } else {
 		testResults = {
       1: {pass: result}
     }
   }
-  
+
   window.parent.postMessage({ type: 'test-results', testResults }, '*');
 }
 
@@ -60,9 +60,13 @@ $('#Run_panelButton', window.parent.document).click(function () {
   setTimeout(() => {
     gameController.codeOrgAPI.resetAttempt();
     gameController.codeOrgAPI.startAttempt(onComplete);
-    brython()
+    try {
+      brython()
+    } catch (err) {
+
+    }
   }, 0)
-  
+
 });
 
 $('#reset_panelButton', window.parent.document).click(function () {
